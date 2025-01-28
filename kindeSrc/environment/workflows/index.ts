773996d8eb018a40b6170b8d8cc8dbe07a8b0c01
Ages <agesmugnia@gmail.com>
2025-01-28 20:17:17 +0700

    update workflow

--- kindeSrc/environment/workflows/index.ts
+++ kindeSrc/environment/workflows/index.ts
@@ -1,9 +1,7 @@
 import passwordStrengthWorkflow from './workflow/PasswordStrengthWorkflow';
-// Import additional workflows here
 
 const workflows = [
   passwordStrengthWorkflow,
-  // Add other workflows as needed
 ];
 
 export default workflows;
