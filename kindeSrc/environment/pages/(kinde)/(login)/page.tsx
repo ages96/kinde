a864abe0b4f8ce79e472d138a72007284fb972c8
Ages M <agesmugnia@gmail.com>
2025-04-08 11:21:15 +0700

    Update page.tsx

--- kindeSrc/environment/pages/(kinde)/(login)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(login)/page.tsx
@@ -2,12 +2,12 @@
 
 import React from "react";
 import { renderToString } from "react-dom/server.browser";
-import { getKindeWidget } from "@kinde/infrastructure";
+import { getKindeWidget, getEnvironmentVariable } from "@kinde/infrastructure";
 
 import { EntryPageHeader } from "../../components/EntryPageHeader";
 import { NavTabs } from "../../components/NavTabs";
 import { Layout } from "../../components/Layout";
-
+const AGES_TEST_ENV = getEnvironmentVariable('AGES_TEST_ENV')?.value
 const PageLayout = async ({ request, context }) => {
   return (
     <Layout request={request} context={context}>
@@ -16,7 +16,7 @@ const PageLayout = async ({ request, context }) => {
           <EntryPageHeader logoAltText={context.widget.content.logoAlt} />
           <NavTabs activeTab="login" />
           
-          <a href={`${process.env.KINDE_SITE_URL}`}>Check with dynamic link</a>
+          <a href={`${AGES_TEST_ENV}`}>Check with dynamic link</a>
           <div>{getKindeWidget()}</div>
         </div>
       </main>
